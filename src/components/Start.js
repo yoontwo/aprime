1cba6a427c0fe716c5c4847e225591661286a5fd
yoontwo <61463592+yoontwo@users.noreply.github.com>
2021-09-20 23:42:52 +0900

    2차

--- src/components/Start.js
+++ src/components/Start.js
@@ -17,9 +17,9 @@ const Start = ({ onQuizStart }) => {
   return (
     <div className="div">
       <div calssName="div_start">
-        <p className="logo_letter">a prime</p>
-
-        <h1 className="Title">나에게 딱 맞는 클린뷰티 제품은?</h1>
+        <p className="logo">a prime</p>
+        <h1 className="startTitle">나에게 딱 맞는 클린뷰티 제품은?</h1>
+        <p>클린뷰티에 대한 간단한 설명추가하기</p>
         <img className="start_img" src={require("./img/start.png").default} />
         <p className="body">테스트 후 응모하면 상품까지!!</p>
         <button className="start_button" onClick={onQuizStart}>
